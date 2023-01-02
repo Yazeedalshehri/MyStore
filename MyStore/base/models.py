99ae141fc9f4b2123eddaf7687f61869ba7ebad8
iHatansa <hatansa77@gmail.com>
2023-01-02 03:06:35 +0300

    DB+urls update

--- MyStore/base/models.py
+++ MyStore/base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -6,5 +7,29 @@ from django.db import models
 class Admins(models.Model):
     username = models.CharField(max_length=15)
     email = models.CharField(max_length=50)
-    password = models.CharField(max_length=17)
+    password = models.CharField(max_length=15)
     phonenumber = models.IntegerField()
+
+
+    slug = models.SlugField(blank=True , null=True)
+
+    def __str__(self) :
+        return  self.username
+
+    def save(self , *args ,**kwargs):
+        if not self.slug :
+            self.slug = slugify(self.username)
+        super(Admins ,self).save(*args ,**kwargs)
+
+
+class product(models.Model):
+     Admins = models.ForeignKey(Admins, on_delete=models.CASCADE)
+     PRDname = models.CharField(max_length=15)
+     PRDnumber = models.IntegerField()
+     quantity = models.IntegerField()
+     PRDimage = models.ImageField(upload_to='images',null=True )
+     price = models.DecimalField(max_digits=10, decimal_places=2)
+     desc = models.CharField(max_length=100)
+
+     def __str__(self) :
+        return  self.PRDname
--- MyStore/base/views.py
+++ MyStore/base/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect , get_object_or_404
 from django.http import request
-from .models import Admins
+from django.contrib.auth.forms import UserCreationForm , PasswordChangeForm
+from django.contrib.auth import login , authenticate
+from django.core.paginator import Paginator
+from .models import Admins , product
 
 
 def HomePage(request):
-    return render(request,"HomePage.html")
+    context = {'Admins' : Admins}
+    return render(request,"HomePage.html",context)
 
 
-def Register(request):
+def Register(request ):
     if request.method == 'POST' :
        username = request.POST['username'] 
        email = request.POST['email'] 
@@ -15,29 +19,54 @@ def Register(request):
        phonenumber = request.POST['phonenumber'] 
        new_admin = Admins(username=username, email=email, password=password,phonenumber=phonenumber)
        new_admin.save()
+        
+       return redirect (AdminPage , slug = username)
+
     return render(request,"Register.html")
 
 
 def Login(request):
     return render(request,"login.html")
 
-def AdminPage(request):
-    return render(request,"AdminPage.html")
+def AdminPage(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    context = {'admin': AdminPage}
+    return render(request,"AdminPage.html", context)
 
-def Analytics(request):
-    return render(request,"Analytics.html")
+def Analytics(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    context = {'admin': AdminPage}
+    return render(request,"Analytics.html",context)
 
-def Customers(request):
-    return render(request,"Customers.html")
-
-
-def Orders(request):
-    return render(request,"Orders.html")
-
-
-def Products(request):
-    return render(request,"Products.html")
+def Customers(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    context = {'admin': AdminPage}
+    return render(request,"Customers.html",context)
 
 
-def Settings(request):
-    return render(request,"Settings.html")
+def Orders(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    context = {'admin': AdminPage}
+    return render(request,"Orders.html",context)
+
+
+def Products(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    pro=product.objects.all()
+    context = {'admin': AdminPage , 'prod': pro}
+    if request.method == 'POST' :
+        Name = request.POST['Name'] 
+        Number = request.POST['Number'] 
+        Quantity = request.POST['Quantity'] 
+        Price = request.POST['Price']   
+        Description = request.POST['Description']  
+        img = request.POST['img']   
+        new_products= product(Admins=AdminPage ,PRDname=Name,PRDnumber=Number,quantity=Quantity,price=Price,desc=Description,PRDimage=img)
+        new_products.save()
+    return render(request,"Products.html",context)
+
+
+def Settings(request , slug):
+    AdminPage= get_object_or_404(Admins , slug=slug)
+    context = {'admin': AdminPage}
+    return render(request,"Settings.html",context)
