e1bf996819704bcd472dc3c90e54f8dae44e3491
LAPTOP-2BUHBQCG\yaaz4 <yaaz436@gmail.com>
2023-01-18 16:22:20 +0300

    AddUserModel&FixBugs_2

--- MyStore/MyStore/urls.py
+++ MyStore/MyStore/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base import views
+from base import views 
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -34,7 +34,7 @@ urlpatterns = [
     path('<slug:slug>/Settings/',views.Settings),
     path('<slug:slug>/store/',views.Store),
     path('<slug:slug>/store/Cart/',views.Cart),
-    path('<slug:slug>/store/login',views.userlogin),
+    path('<slug:slug>/store/login',views.UserLoginView),
    
     
 
--- MyStore/base/views.py
+++ MyStore/base/views.py
@@ -3,12 +3,13 @@ from django.http import request
 from django.contrib.auth.forms import UserCreationForm , PasswordChangeForm
 from django.contrib.auth import login , authenticate
 from django.core.paginator import Paginator
-from .models import Admins , product , order
+from .models import Admins , product , order ,User,Cart
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import AdminsForm
+from .forms import AdminsForm,UserLoginForm
 from django.views.generic import CreateView 
+
 
 
 def HomePage(request):
@@ -89,9 +90,11 @@ def Customers(request , slug):
 def Orders(request , slug):
     AdminPage= get_object_or_404(Admins , slug=slug)
     ords=order.objects.all()
-    context = {'admin': AdminPage , 'ords' : ords}
-    return render(request,"Orders.html",context)
-
+    for i in ords :
+         if i.Admins.slug == AdminPage.slug:
+            print(i.products.values_list())
+            print("hh")
+    return render(request, 'Orders.html')        
 
 def Products(request , slug):
     AdminPage= get_object_or_404(Admins , slug=slug)
@@ -115,15 +118,30 @@ def Settings(request , slug):
     if request.POST.get('StoreColor'):
         AdminPage.storecolor=request.POST.get('StoreColor')
         AdminPage.save()
+    if request.POST.get('StoreTextColor'):
+        AdminPage.StoreTextColor=request.POST.get('StoreTextColor')
+        AdminPage.save()    
     if request.POST.get('StoreName'):
         AdminPage.storename=request.POST.get('StoreName')
         AdminPage.save()
         
     return render(request,"Settings.html",context)
 
-def Store(request , slug):
+def Store(request , slug ):
     AdminPage= get_object_or_404(Admins , slug=slug)
+    
     pro=product.objects.all()
+    
+
+    if request.method == 'POST':
+        admin = 'yazeed'
+        user = 'yazed'
+        productCart = request.POST.get('Product')
+        price = request.POST.get('Price')
+        new_cart = Cart(Admin = admin ,User = user, Product= productCart,Total=price)
+        new_cart.save() 
+       
+    
     context = {'admin': AdminPage , 'prod': pro}
     return render(request,"carstore.html",context)
 
@@ -133,7 +151,22 @@ def Cart(request , slug):
     return render(request,"CartPage.html",context )
 
 def userlogin(request , slug):
+   AdminPage= get_object_or_404(Admins , slug=slug)
+   context = {'admin': AdminPage}
+   return render(request,"UserLogin.html",context)
+
+def UserLoginView(request ,slug):
     AdminPage= get_object_or_404(Admins , slug=slug)
-    context = {'admin': AdminPage}
-    return render(request,"UserLogin.html",context)
+    if request.method == 'POST':
+        Name = request.POST['name'] 
+        Email = request.POST['email'] 
+        password = request.POST['password'] 
+        Phonenumber = request.POST['phonenumber']   
+        new_User = User(Name= Name, Email= Email, password = password, Phonenumber= Phonenumber)
+        new_User.save()  
+        
+    Store(request,slug,Name)    
+
+        
+    return render(request,"UserLogin.html")
     
