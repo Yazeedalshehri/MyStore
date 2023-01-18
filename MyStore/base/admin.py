673756b70c0c833d7c233078c1b3438bcb9df29d
LAPTOP-2BUHBQCG\yaaz4 <yaaz436@gmail.com>
2023-01-18 15:39:29 +0300

    AddUserModel&FixBugs

--- MyStore/base/admin.py
+++ MyStore/base/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Admins , product , order
+from .models import Admins , product , order, User, Cart
 
 
 
 admin.site.register(Admins)
 admin.site.register(product)
 admin.site.register(order)
+admin.site.register(User)
+admin.site.register(Cart)
